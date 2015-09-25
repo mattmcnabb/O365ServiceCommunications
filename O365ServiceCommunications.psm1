46eb45e8fdf03c7d9f1b8670ebe9ea75b065feb9
Matt McNabb <mcnabbmh@gmail.com>
2015-09-25 15:14:56 -0400

    Fix dot-sourcing issue

--- O365ServiceCommunications.psm1
+++ O365ServiceCommunications.psm1
@@ -1,8 +1,10 @@
 $PublicScripts = Get-ChildItem -Path "$PSScriptRoot\Scripts\Public" -File -Filter *.ps1
 $PrivateScripts = Get-ChildItem -Path "$PSScriptRoot\Scripts\Private" -File -Filter *.ps1
 
-$PublicScripts,$PrivateScripts | foreach {
-	. $_.FullName
+
+foreach ($Script in ($PublicScripts + $PrivateScripts))
+{
+	. $Script.FullName
 }
 
 $Commands = $PublicScripts.BaseName
