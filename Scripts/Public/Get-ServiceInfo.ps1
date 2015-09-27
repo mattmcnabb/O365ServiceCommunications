2b0347d7d52f426f281a9e89e2c32f6591fd925c
Matt McNabb <mmcnabb@outlook.com>
2015-09-27 16:00:15 -0400

    Update Get-ServiceInfo to working state

--- Scripts/Public/Get-ServiceInfo.ps1
+++ Scripts/Public/Get-ServiceInfo.ps1
@@ -3,12 +3,24 @@ function Get-ServiceInfo
     [CmdletBinding()]
     param
     (
+		[Parameter(Mandatory)]
+        [PSTypeName('O365ServiceCommunications.Session')]
+        $SCSession
     )
 
+    $Body = @{
+        lastCookie = $SCSession.Cookie
+        locale     = $SCSession.Locale
+    }
 
-    # TODO: place your function code here
-    # this code gets executed when the function is called
-    # and all parameters have been processed
+    $Splat = @{
+        ContentType = 'application/json'
+        Method      = 'Post'
+        Uri         = 'https://api.admin.microsoftonline.com/shdtenantcommunications.svc/GetServiceInformation'
+		Body        = $Body | ConvertTo-Json
+    }
 
-
+	Invoke-RestMethod @Splat | foreach {
+		$_ | New-CustomObject -TypeName $ServiceInfoTypeName
+	}
 }
