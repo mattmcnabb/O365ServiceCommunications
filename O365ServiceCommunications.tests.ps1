8e15d5014ccdb88031b48ec5491c86025cc379f2
Matt McNabb <mmcnabb@outlook.com>
2015-09-29 23:03:01 -0400

    Add initial Pester tests

--- O365ServiceCommunications.tests.ps1
+++ O365ServiceCommunications.tests.ps1
@@ -1,13 +1,25 @@
-#
-# This is a PowerShell Unit Test file.
-# You need a unit test framework such as Pester to run PowerShell Unit tests. 
-# You can download Pester from http://go.microsoft.com/fwlink/?LinkID=534084
-#
+$ModulePath = Split-Path -Parent $MyInvocation.MyCommand.Path
+$ModuleName = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -Replace ".Tests.ps1"
 
-Describe "Get-Function" {
-	Context "Function Exists" {
-		It "Should Return" {
-		
-		}
-	}
+$ManifestPath   = "$ModulePath\$ModuleName.psd1"
+
+Describe "Manifest" {
+    $Manifest = $null
+    It "has a valid manifest" {
+        {
+            $Script:Manifest = Test-ModuleManifest -Path $ManifestPath -ErrorAction Stop -WarningAction SilentlyContinue
+        } | Should Not Throw
+    }
+
+    It "has a valid name in the manifest" {
+        $Script:Manifest.Name | Should Be $ModuleName
+    }
+
+    It "has a valid guid in the manifest" {
+        $Script:Manifest.Guid | Should Be 'bd4390dc-a8ad-4bce-8d69-f53ccf8e4163'
+    }
+
+    It "has a valid version in the manifest" {
+        $Script:Manifest.Version -as [Version] | Should Not BeNullOrEmpty
+    }
 }
