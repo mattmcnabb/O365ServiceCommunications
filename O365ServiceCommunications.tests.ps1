50d7e026537aecf0613bb0d3e2c99807e6e1d7fe
Matt McNabb <mcnabbmh@gmail.com>
2015-09-30 10:25:48 -0400

    Update tests

--- O365ServiceCommunications.tests.ps1
+++ O365ServiceCommunications.tests.ps1
@@ -2,7 +2,9 @@ $ModulePath = Split-Path -Parent $MyInvocation.MyCommand.Path
 $ModuleName = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -Replace ".Tests.ps1"
 
 $ManifestPath   = "$ModulePath\$ModuleName.psd1"
+Import-Module $ManifestPath
 
+# test the module manifest - exports the right functions, processes the right formats, and is generally correct
 Describe "Manifest" {
     $Manifest = $null
     It "has a valid manifest" {
@@ -22,4 +24,22 @@ Describe "Manifest" {
     It "has a valid version in the manifest" {
         $Script:Manifest.Version -as [Version] | Should Not BeNullOrEmpty
     }
+
+	It 'processes all existing format files' {
+		$FormatFiles = Get-ChildItem "$ModulePath\Formats\" -Filter *.xml | select -ExpandProperty fullname
+		$Script:Manifest.ExportedFormatFiles | Should Be $FormatFiles
+	}
+
+	It 'exports all public functions' {
+		$FunctionFiles = Get-ChildItem "$ModulePath\Scripts\Public" -Filter *.ps1 | select -ExpandProperty basename
+		$FunctionNames = $FunctionFiles | foreach {$_ -replace '-', "-$($Script:Manifest.Prefix)"}
+		$Script:Manifest.ExportedFunctions.Values.Name | Should Be $FunctionNames
+	}
 }
+
+# test the functions inside the module
+#describe 'Module' {
+#	InModuleScope O365ServiceCommunications {
+#		It ''
+#	}
+#}
