d650cba1d33fdf0d5aa5d9a702930b2babb424f8
Matt McNabb <mmcnabb@outlook.com>
2016-01-21 22:41:13 -0500

    Add basic tests for all exported functions

--- O365ServiceCommunications.tests.ps1
+++ O365ServiceCommunications.tests.ps1
@@ -17,11 +17,11 @@ Describe "Manifest" {
         $Script:Manifest.Name | Should Be $ModuleName
     }
 
-	It "has a valid root module" {
+    It "has a valid root module" {
         $Script:Manifest.RootModule | Should Be "$ModuleName.psm1"
     }
 
-	It "has a valid Description" {
+    It "has a valid Description" {
         $Script:Manifest.Description | Should Not BeNullOrEmpty
     }
 
@@ -33,28 +33,28 @@ Describe "Manifest" {
         $Script:Manifest.Version -as [Version] | Should Not BeNullOrEmpty
     }
 
-	It "has a valid prefix" {
-		$Script:Manifest.Prefix | Should Not BeNullOrEmpty
-	}
+    It "has a valid prefix" {
+        $Script:Manifest.Prefix | Should Not BeNullOrEmpty
+    }
 
-	It "has a valid copyright" {
-		$Script:Manifest.CopyRight | Should Not BeNullOrEmpty
-	}
+    It "has a valid copyright" {
+        $Script:Manifest.CopyRight | Should Not BeNullOrEmpty
+    }
 
-	It 'processes all existing format files' {
-		$FormatFiles = Get-ChildItem "$ModulePath\Formats\" -Filter *.ps1xml | select -ExpandProperty fullname
-		$Script:Manifest.ExportedFormatFiles | Should Be $FormatFiles
-	}
+    It 'processes all existing format files' {
+        $FormatFiles = Get-ChildItem "$ModulePath\Formats\" -Filter *.ps1xml | select -ExpandProperty fullname
+        $Script:Manifest.ExportedFormatFiles | Should Be $FormatFiles
+    }
 
-	It 'exports all public functions' {
-		$FunctionFiles = Get-ChildItem "$ModulePath\functions" -Filter *.ps1 | select -ExpandProperty basename
-		$FunctionNames = $FunctionFiles | foreach {$_ -replace '-', "-$($Script:Manifest.Prefix)"}
-		$Script:Manifest.ExportedFunctions.Values.Name | Should Be $FunctionNames
-	}
+    It 'exports all public functions' {
+        $FunctionFiles = Get-ChildItem "$ModulePath\functions" -Filter *.ps1 | select -ExpandProperty basename
+        $FunctionNames = $FunctionFiles | foreach {$_ -replace '-', "-$($Script:Manifest.Prefix)"}
+        $Script:Manifest.ExportedFunctions.Values.Name | Should Be $FunctionNames
+    }
 }
 
 # test the functions inside the module
-Describe 'Module' {
+Describe 'helper functions' {
     InModuleScope O365ServiceCommunications {
         Context 'New-CustomObject' {
             BeforeEach {
@@ -102,10 +102,15 @@ Describe 'Module' {
                 $Object.Property4.GetType().BaseType.FullName | Should Be 'System.Array'
             }
         }
+    }
+}
 
+Describe 'public functions' {
+    InModuleScope O365ServiceCommunications {
+        $TestCred = New-Object System.Management.Automation.PSCredential ('testUser', (ConvertTo-SecureString '1234abcD' -AsPlainText -Force))
+        
         Context 'New-Session'{
             BeforeEach {
-                $TestCred = New-Object System.Management.Automation.PSCredential ('testUser', (ConvertTo-SecureString '1234abcD' -AsPlainText -Force))
                 $CookieText = 'qwertyuiopasdfghjkl;'
             }
 
@@ -113,6 +118,58 @@ Describe 'Module' {
                 Mock Invoke-RestMethod {[pscustomobject]@{RegistrationCookie = $CookieText}}
                 $Result = New-SCSession -Credential $TestCred
                 $Result.PsObject.TypeNames[0] | Should Be $SessionTypeName
+            }
+        }
+        
+        Context 'Get-ServiceInfo' {
+            BeforeEach {
+                Mock New-Session { [pscustomobject]@{Cookie = 'qwertyuiopasdfghjkl;'; Locale = 'en-US'} | New-CustomObject -TypeName $SessionTypeName }
+                Mock Invoke-RestMethod {[PSCustomObject]@{ ServiceInfo = 'ServiceInfoTest' }}
+            }
+        
+            It 'outputs a service info object' {
+                $Session = New-Session -Credential $TestCred
+                $Result = Get-ServiceInfo -SCSession $Session
+                $Result.PSObject.TypeNames[0] | Should Be $ServiceInfoTypeName
+            }
+        }
+        
+        Context 'Get-Event' {
+            BeforeEach {
+                Mock New-Session { [pscustomobject]@{Cookie = 'qwertyuiopasdfghjkl;'; Locale = 'en-US'} | New-CustomObject -TypeName $SessionTypeName }
+                Mock Invoke-RestMethod {[PSCustomObject]@{ Events = 'EventTest' }}
+            }
+        
+            It 'outputs an event object' {
+                $Session = New-Session -Credential $TestCred
+                $Result = Get-Event -SCSession $Session
+                $Result.PSObject.TypeNames[0] | Should Be $EventTypeName
+            }
+        }
+        
+        Context 'Get-TenantServiceInfo' {
+            BeforeEach {
+                Mock New-Session { [pscustomobject]@{Cookie = 'qwertyuiopasdfghjkl;'; Locale = 'en-US'} | New-CustomObject -TypeName $SessionTypeName }
+                Mock Invoke-RestMethod {[PSCustomObject]@{ TenantServiceInfo = 'TenantServiceInfoTest' }}
+            }
+        
+            It 'outputs an event object' {
+                $Session = New-Session -Credential $TestCred
+                $Result = Get-TenantServiceInfo -SCSession $Session -Domains domain.com
+                $Result.PSObject.TypeNames[0] | Should Be $TenantServiceInfoTypeName
+            }
+        }
+        
+        Context 'Get-TenantEvent' {
+            BeforeEach {
+                Mock New-Session { [pscustomobject]@{Cookie = 'qwertyuiopasdfghjkl;'; Locale = 'en-US'} | New-CustomObject -TypeName $SessionTypeName }
+                Mock Invoke-RestMethod {[PSCustomObject]@{ Events = 'TenantEventTest' }}
+            }
+        
+            It 'outputs an event object' {
+                $Session = New-Session -Credential $TestCred
+                $Result = Get-TenantEvent -SCSession $Session  -Domains domain.com
+                $Result.PSObject.TypeNames[0] | Should Be $EventTypeName
             }
         }
     }
