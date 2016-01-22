92de1f8ab6b55d03a8a2a2b11e356210a49d7adb
Matt McNabb <mmcnabb@outlook.com>
2016-01-21 22:42:01 -0500

    Remove TenantEventTypeName - not needed

--- helpers/Config.ps1
+++ helpers/Config.ps1
@@ -1,5 +1,4 @@
 $SessionTypeName = 'O365ServiceCommunications.Session'
 $EventTypeName   = 'O365ServiceCommunications.Event'
-$TenantEventTypeName   = 'O365ServiceCommunications.TenantEvent'
 $TenantServiceInfoTypeName = 'O365ServiceCommunications.TenantServiceInfo'
 $ServiceInfoTypeName   = 'O365ServiceCommunications.ServiceInfo'
