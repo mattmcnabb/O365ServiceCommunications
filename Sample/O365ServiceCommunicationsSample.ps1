4d7dbb199bf2b0979167c86bd4981aaf26d9c547
Matt McNabb <mmcnabb@outlook.com>
2016-02-11 09:08:45 -0500

    Add comment help to the sample script

--- Sample/O365ServiceCommunicationsSample.ps1
+++ Sample/O365ServiceCommunicationsSample.ps1
@@ -1,4 +1,18 @@
-﻿Import-Module O365ServiceCommunications
+﻿<#
+    .DESCRIPTION
+    This sample script demonstrates using the O365ServiceCommunications module to alert on incidents posted in the Office 365 
+    Message Center. It gathers information from the Service Communications API and builds custom HTML tables which are then 
+    added to an email message body. This script can be set to run on a schedule using the Windows Task Scheduler to make sure 
+    you are aware that an incident has been generated.
+
+    Don't forget to save a credential object to file in the same directory where this script is saved. This is required for 
+    authentication to the API.
+
+    You'll need to change the values of the $Splat variable below to something that will work in your environment to successfully 
+    send and email message.
+#>
+
+Import-Module O365ServiceCommunications
 
 # Import a credential object to use against the Service Communications API
 # this needs to be a global admin for your Office 365 tenant
@@ -64,7 +78,7 @@ if ($Events)
         </body>
     </html>
 "@
-    
+
     $Splat = @{
         SmtpServer  = 'servername.domain.local'
         Body        = $Html
